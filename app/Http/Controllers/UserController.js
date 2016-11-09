2ed75def6a12a02313633c2cb41a3ee8d3efc000
“Shaun <shaunwmusic@gmail.com>
2016-11-09 16:45:16 -0600

    Store user when register is posted to

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -9,7 +9,33 @@ class UserController {
   }
 
   * store(request, response) {
+    const { username, email, password } = request.all();
 
+  try {
+    // Save the user
+    const user = yield User.create( {
+      username,
+      email,
+      password: yield Hash.make(password),
+    });
+
+    yield request.auth.login(user);
+    //login
+    yield request.with({
+      success: 'Congrats on your new account!',
+    }).flash();
+    //give them a success message
+    //redirect them to the main site
+    response.redirect('/users');
+
+  } catch(e) {
+    yield request
+      .withOut('password') // shows us the old input values
+      .andWith({ error: 'That username or email is already taken' })
+      .flash(); //Makes this data only last for one request
+
+    response.redirect('back');
+  }
   }
 
 
