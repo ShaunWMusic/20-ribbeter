c13579d7c6d57a8fb35ca41e8402e1aefbbad270
“Shaun <shaunwmusic@gmail.com>
2016-11-11 15:03:01 -0600

    Styling /app 'see what's happening section

--- resources/javascript/routes/index.vue
+++ resources/javascript/routes/index.vue
@@ -4,7 +4,7 @@
     <div class="grid">
       <div class="grid__item">
       <div class="card">
-          <h2>New Ribbit</h2>
+          <h2 class="card__header">New Ribbit</h2>
           <div class="grid__item--input">
         <p>What's Going On?</p>
         <textarea v-model="message" name="name" rows="8" cols="40"></textarea>
@@ -15,18 +15,21 @@
               </div>
       </div>
       </div>
-      <div class="grid__item">
-        <h2>See What's Happening!</h2>
-      <div class="grid-item__results">
+      <div class="grid__item--main">
+        <div class="grid__item--ribbits">
+          <h2>See What's Happening!</h2>
+        </div>
+      <div class="grid__item--results">
         <p>Load New Ribbits</p>
-        <div class="grid-item__ribbits">
-        <ul>
+      </div>
+        <div class="grid__item--ribbits">
+        <ul class="grid__item--post">
           <li v-for="post in posts">
             {{ post.username }}
+            {{ post.comment }}
           </li>
         </ul>
         </div>
-      </div>
       </div>
     </div>
   </div>
